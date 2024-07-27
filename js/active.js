f6915715cd1ef9afd2e28ac42f50b33cc6484b32
Monoget <monoget2@gmail.com>
2024-07-27 16:36:26 +0600

    update all

--- js/active.js
+++ js/active.js
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------
-// Template Name: Affan - PWA Mobile HTML Template
+// Template Name: FrogBID - Web App
 // Template Author: Designing World
 // Template Author URL: https://themeforest.net/user/designing-world
 // Last Updated: 23 Dec, 2022
